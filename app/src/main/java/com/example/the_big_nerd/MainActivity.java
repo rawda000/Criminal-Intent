6c126aeb764bca741196d7cc75b6cf07a63f00fc
Rawda Yasser <rawdayasser2019@gmail.com>
2020-11-22 07:06:01 +0200

    first commit

---
+++ app/src/main/java/com/example/the_big_nerd/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.the_big_nerd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
